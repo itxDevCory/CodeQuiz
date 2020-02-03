ad320d15c5f4f4cf23f1eb663a2ccf5ccc67b531
Cory Suida <itxdevcory@gmail.com>
2020-02-03 11:14:53 -0500

    finalized project, rough but limited on time

--- quiz.js
+++ quiz.js
@@ -1,24 +1,91 @@
-//Complete the loop, indicate when the game is over//
-//Cory workout the timer for these questions///
-
-function quiz(questions) {
+function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
-    this.currentQuestionIndex = 0;
+    this.questionIndex = 0;
 }
-quiz.prototype.getQuestionIndex = function() {
- return this.questions[this.questionIndex]
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
 }
-   
-};
-Quiz.prototype.isEnded = function() {
-    return this.questions.length ===this.QuestionIndex;
-};
-
+ 
 Quiz.prototype.guess = function(answer) {
-    if (this.getQuestionIndex().correctAnswer(answer)) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
         this.score++;
     }
+ 
     this.questionIndex++;
 }
-    
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+ 
+ 
+class Question {
+    constructor(text, choices, answer) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+    }
+    isCorrectAnswer(choice) {
+        return this.answer === choice;
+    }
+}
+ 
+ 
+ 
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        };
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+ 
+
+ 
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+ 
+// create questions here
+var questions = [
+    new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
+    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
+    new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
+    new Question("What does JSON stand for?", ["Juxtaposed Script ON NETWORKn", "Javascript Object Notation"], "Javascript Object Notation")
+];
+ 
+// create quiz
+var quiz = new Quiz(questions);
+ 
+// display quiz
+populate();
+        
+
+//-->
+ 
